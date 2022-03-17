b5ba36200c277f2d0de992814f495b885bb876e6
jwolfy <84108269+jwolfy@users.noreply.github.com>
2022-03-17 21:25:57 +0300

    Add logback

--- src/main/java/grocerymanager/service/ProductServiceImpl.java
+++ src/main/java/grocerymanager/service/ProductServiceImpl.java
@@ -2,6 +2,8 @@ package grocerymanager.service;
 
 import grocerymanager.model.Product;
 import grocerymanager.repository.ProductRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
@@ -13,6 +15,7 @@ import java.util.List;
 @Service
 @Transactional(isolation = Isolation.READ_COMMITTED)
 public class ProductServiceImpl implements ProductService {
+    private static final Logger log = LoggerFactory.getLogger(ProductServiceImpl.class);
     private final ProductRepository productRepository;
 
     @Autowired
@@ -22,11 +25,13 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public List<Product> findAll() {
+        log.info("Fetching all products");
         return productRepository.findAll();
     }
 
     @Override
     public Product findById(int id) {
+        log.info("Fetching product by id: {} ", id);
         return productRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Product not found by id " + id));
 
@@ -34,18 +39,21 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public Product save(Product product) {
+        log.info("Saving product: {} ", product);
         return productRepository.save(product);
     }
 
     @Override
     public void deleteById(int id) {
         productRepository.deleteById(id);
+        log.info("Product with id: {} deleted", id);
     }
 
     @Override
     public Product incrementQuantity(int id, int amount) {
         Product product = findById(id);
         product.setQuantity(product.getQuantity() + amount);
+        log.info("Product with id: {} incrementing quantity by {}", id, amount);
         return productRepository.save(product);
     }
 
@@ -54,8 +62,10 @@ public class ProductServiceImpl implements ProductService {
         Product product = findById(id);
         if (product.getQuantity() >= amount) {
             product.setQuantity(product.getQuantity() - amount);
+            log.info("Product with id: {} decrementing quantity by {}", id, amount);
             return productRepository.save(product);
         } else {
+            log.warn("Attempted to set negative quantity for product with id: {}", id);
             throw new RuntimeException("Negative quantity is not allowed");
         }
     }
